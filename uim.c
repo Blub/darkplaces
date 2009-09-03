08201935f4e3bb254c425459937c50fd90c811f1
Wolfgang (Blub) Bumiller <blub@speed.at>
2009-09-03 20:50:49 +0200

    started with UIM support

---
+++ uim.c
@@ -0,0 +1,137 @@
+/* UIM input support for
+ * DarkPlaces
+ */
+#include "quakedef.h"
+
+#include "uim_dp.h"
+
+/*
+================================================================================
+CVars introduced with the UIM extension
+================================================================================
+*/
+
+cvar_t im_method = {CVAR_SAVE, "im_method", "anthy", "which input method to use if uim is supported and active"};
+cvar_t im_enabled = {CVAR_SAVE, "im_enabled", "0", "use UIM input"};
+
+/*
+================================================================================
+Library imports. Taken from the uim headers.
+================================================================================
+*/
+
+struct uim_code_converter *quim_iconv;
+
+int           (*quim_init)(void);
+void          (*quim_quit)(void);
+uim_candidate (*quim_get_candidate)(uim_context, int index, int accel_enum_hint);
+void          (*quim_candidate_free)(uim_candidate);
+int           (*quim_get_candidate_index)(uim_context);
+const char*   (*quim_candidate_get_cand_str)(uim_candidate);
+const char*   (*quim_get_default_im_name)(const char *localename);
+uim_context   (*quim_create_context)(void *cookie,
+				     const char *encoding,
+				     const char *im, const char *lang,
+				     const char *engine,
+				     struct uim_code_converter *conv,
+				     void (*commit_cb)(void *ptr, const char *str));
+void          (*quim_release_context)(uim_context);
+int           (*quim_helper_init_client_fd)(void (*disconnect_cb)(void));
+void          (*quim_close_client_fd)(int);
+void          (*quim_set_preedit_cb)(uim_context,
+				     void (*clear_cb)(void*),
+				     void (*pushback_cb)(void*, int attr, const char *str),
+				     void (*update_cb)(void*));
+//void          (*quim_set_prop_list_update_cb)(uim_context,
+//					      void (*update_cb)(void*, const char *longstr));
+void          (*quim_set_candidate_selector_cb)(uim_context,
+						void (*activate_cb)(void*, int nr, int display_limit),
+						void (*select_cb)(void*, int index);
+						void (*shift_page_cb)(void*, int direction),
+						void (*deactivate_cb)(void*));
+void          (*quim_prop_list_update)(uim_context);
+void          (*quim_press_key)(uim_context, int key, int mods);
+void          (*quim_release_key)(uim_context, int key, int mods);
+
+/*
+================================================================================
+Support for dynamically loading the UIM library
+================================================================================
+*/
+
+static dllfunction_t uimfuncs[] =
+{
+	{"uim_init",			(void **) &quim_init},
+	{"uim_quit",			(void **) &quim_quit},
+	{"uim_get_candidate",		(void **) &quim_get_candidate},
+	{"uim_candidate_free",		(void **) &quim_candidate_free},
+	{"uim_get_candidate_index",	(void **) &quim_get_candidate_index},
+	{"uim_candidate_get_cand_str",	(void **) &quim_candidate_get_cand_str},
+	{"uim_get_default_im_name",	(void **) &quim_get_default_im_name},
+	{"uim_create_context",		(void **) &quim_create_context},
+	{"uim_release_context",		(void **) &quim_release_context},
+	{"uim_helper_init_client_fd",	(void **) &quim_helper_init_client_fd},
+	{"uim_close_client_fd",		(void **) &quim_close_client_fd},
+	{"uim_set_preedit_cb",		(void **) &quim_set_preedit_cb},
+	{"uim_set_candidate_selector_cb",		(void **) &quim_set_candidate_selector_cb},
+	{"uim_prop_list_update",	(void **) &quim_prop_list_update},
+	{"uim_press_key",		(void **) &quim_press_key},
+	{"uim_release_key",		(void **) &quim_release_key},
+	{"uim_iconv",			(void **) &quim_iconv},
+	
+	{NULL, NULL}
+};
+
+/// Handle for UIM dll
+static dllhandle_t uim_dll = NULL;
+
+/// Memory pool for uim
+static mempool_t *uim_mempool= NULL;
+
+/*
+====================
+UIM_CloseLibrary
+
+Unload the UIM DLL
+====================
+*/
+void UIM_CloseLibrary (void)
+{
+	if (uim_mempool)
+		Mem_FreePool(&uim_mempool);
+	Sys_UnloadLibrary (&uim_dll);
+}
+
+/*
+====================
+UIM_OpenLibrary
+
+Try to load the UIM DLL
+====================
+*/
+qboolean UIM_OpenLibrary (void)
+{
+	const char* dllnames [] =
+	{
+#if defined(WIN64)
+		#error path for freetype 2 dll
+#elif defined(WIN32)
+		#error path for freetype 2 dll
+#elif defined(MACOSX)
+		"libuim.dylib",
+#else
+		"libuim.so.6",
+		"libuim.so",
+#endif
+		NULL
+	};
+
+	// Already loaded?
+	if (uim_dll)
+		return true;
+
+	// Load the DLL
+	if (!Sys_LoadLibrary (dllnames, &uim_dll, uimfuncs))
+		return false;
+	return true;
+}
---
+++ uim_dp.h
@@ -0,0 +1,349 @@
+/*
+  This contains parts of the original header and thus contains its copyright notice:
+
+  Copyright (c) 2003-2009 uim Project http://code.google.com/p/uim/
+
+  All rights reserved.
+
+  Redistribution and use in source and binary forms, with or without
+  modification, are permitted provided that the following conditions
+  are met:
+
+  1. Redistributions of source code must retain the above copyright
+     notice, this list of conditions and the following disclaimer.
+  2. Redistributions in binary form must reproduce the above copyright
+     notice, this list of conditions and the following disclaimer in the
+     documentation and/or other materials provided with the distribution.
+  3. Neither the name of authors nor the names of its contributors
+     may be used to endorse or promote products derived from this software
+     without specific prior written permission.
+
+  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS ``AS IS'' AND
+  ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+  ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE LIABLE
+  FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+  DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+  OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+  HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+  OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+  SUCH DAMAGE.
+
+*/
+#ifndef UIM_DP__H__
+#define UIM_DP_H__
+
+typedef enum
+{
+	uim_false = 0,
+	uim_true = 1
+} uim_bool;
+
+typedef struct uim_context_   *uim_context;
+typedef struct uim_candidate_ *uim_candidate;
+
+// from the original uim.h:
+enum UKey {
+  UKey_0 = 48,
+  UKey_1 = 49,
+  UKey_2 = 50,
+  UKey_3 = 51,
+  UKey_4 = 52,
+  UKey_5 = 53,
+  UKey_6 = 54,
+  UKey_7 = 55,
+  UKey_8 = 56,
+  UKey_9 = 57,
+  UKey_Yen = 165,
+
+  UKey_Escape = 256,
+  UKey_Tab,
+  UKey_Backspace,
+  UKey_Delete,
+  UKey_Insert,
+  UKey_Return,
+  UKey_Left,
+  UKey_Up ,
+  UKey_Right ,
+  UKey_Down ,
+  UKey_Prior , /* page up */
+  UKey_Next , /* page down */
+  UKey_Home,
+  UKey_End,
+
+  UKey_Multi_key, /* multi-key character compose */
+  UKey_Codeinput,
+  UKey_SingleCandidate,
+  UKey_MultipleCandidate,
+  UKey_PreviousCandidate,
+  UKey_Mode_switch, /* charcter set switch */
+
+  /* Japanese keyboard */
+  UKey_Kanji, /* kanji, kanji convert */
+  UKey_Muhenkan, /* cancel conversion */
+  UKey_Henkan_Mode, /* start/stop conversion */
+  UKey_Henkan = UKey_Henkan_Mode, /* alias for Henkan_Mode */
+  UKey_Romaji,
+  UKey_Hiragana,
+  UKey_Katakana,
+  UKey_Hiragana_Katakana, /* hiragana/katakana toggle */
+  UKey_Zenkaku,
+  UKey_Hankaku,
+  UKey_Zenkaku_Hankaku, /* zenkaku/hankaku toggle */
+  UKey_Touroku,
+  UKey_Massyo,
+  UKey_Kana_Lock,
+  UKey_Kana_Shift,
+  UKey_Eisu_Shift, /* alphanumeric shift */
+  UKey_Eisu_toggle, /* alphanumeric toggle */
+
+  /* Korean keyboard */
+  UKey_Hangul,
+  UKey_Hangul_Start,
+  UKey_Hangul_End,
+  UKey_Hangul_Hanja,
+  UKey_Hangul_Jamo,
+  UKey_Hangul_Romaja,
+  UKey_Hangul_Codeinput,
+  UKey_Hangul_Jeonja,
+  UKey_Hangul_Banja,
+  UKey_Hangul_PreHanja,
+  UKey_Hangul_PostHanja,
+  UKey_Hangul_SingleCandidate,
+  UKey_Hangul_MultipleCandidate,
+  UKey_Hangul_PreviousCandidate,
+  UKey_Hangul_Special,
+
+  /* function keys */
+  UKey_F1,
+  UKey_F2,
+  UKey_F3,
+  UKey_F4,
+  UKey_F5,
+  UKey_F6,
+  UKey_F7,
+  UKey_F8,
+  UKey_F9,
+  UKey_F10,
+  UKey_F11,
+  UKey_F12,
+  UKey_F13,
+  UKey_F14,
+  UKey_F15,
+  UKey_F16,
+  UKey_F17,
+  UKey_F18,
+  UKey_F19,
+  UKey_F20,
+  UKey_F21,
+  UKey_F22,
+  UKey_F23,
+  UKey_F24,
+  UKey_F25,
+  UKey_F26,
+  UKey_F27,
+  UKey_F28,
+  UKey_F29,
+  UKey_F30,
+  UKey_F31,
+  UKey_F32,
+  UKey_F33,
+  UKey_F34,
+  UKey_F35, /* X, Gtk and Qt supports up to F35 */
+
+  /* dead keys */
+  UKey_Dead_Grave,
+  UKey_Dead_Acute,
+  UKey_Dead_Circumflex,
+  UKey_Dead_Tilde,
+  UKey_Dead_Macron,
+  UKey_Dead_Breve,
+  UKey_Dead_Abovedot,
+  UKey_Dead_Diaeresis,
+  UKey_Dead_Abovering,
+  UKey_Dead_Doubleacute,
+  UKey_Dead_Caron,
+  UKey_Dead_Cedilla,
+  UKey_Dead_Ogonek,
+  UKey_Dead_Iota,
+  UKey_Dead_VoicedSound,
+  UKey_Dead_SemivoicedSound,
+  UKey_Dead_Belowdot,
+  UKey_Dead_Hook,
+  UKey_Dead_Horn,
+
+  /* Japanese Kana keys */
+  UKey_Kana_Fullstop,
+  UKey_Kana_OpeningBracket,
+  UKey_Kana_ClosingBracket,
+  UKey_Kana_Comma,
+  UKey_Kana_Conjunctive,
+  UKey_Kana_WO,
+  UKey_Kana_a,
+  UKey_Kana_i,
+  UKey_Kana_u,
+  UKey_Kana_e,
+  UKey_Kana_o,
+  UKey_Kana_ya,
+  UKey_Kana_yu,
+  UKey_Kana_yo,
+  UKey_Kana_tsu,
+  UKey_Kana_ProlongedSound,
+  UKey_Kana_A,
+  UKey_Kana_I,
+  UKey_Kana_U,
+  UKey_Kana_E,
+  UKey_Kana_O,
+  UKey_Kana_KA,
+  UKey_Kana_KI,
+  UKey_Kana_KU,
+  UKey_Kana_KE,
+  UKey_Kana_KO,
+  UKey_Kana_SA,
+  UKey_Kana_SHI,
+  UKey_Kana_SU,
+  UKey_Kana_SE,
+  UKey_Kana_SO,
+  UKey_Kana_TA,
+  UKey_Kana_CHI,
+  UKey_Kana_TSU,
+  UKey_Kana_TE,
+  UKey_Kana_TO,
+  UKey_Kana_NA,
+  UKey_Kana_NI,
+  UKey_Kana_NU,
+  UKey_Kana_NE,
+  UKey_Kana_NO,
+  UKey_Kana_HA,
+  UKey_Kana_HI,
+  UKey_Kana_FU,
+  UKey_Kana_HE,
+  UKey_Kana_HO,
+  UKey_Kana_MA,
+  UKey_Kana_MI,
+  UKey_Kana_MU,
+  UKey_Kana_ME,
+  UKey_Kana_MO,
+  UKey_Kana_YA,
+  UKey_Kana_YU,
+  UKey_Kana_YO,
+  UKey_Kana_RA,
+  UKey_Kana_RI,
+  UKey_Kana_RU,
+  UKey_Kana_RE,
+  UKey_Kana_RO,
+  UKey_Kana_WA,
+  UKey_Kana_N,
+  UKey_Kana_VoicedSound,
+  UKey_Kana_SemivoicedSound,
+
+  /* non-standard platform specific keys (e.g. Zaurus PDA) */
+  UKey_Private1,
+  UKey_Private2,
+  UKey_Private3,
+  UKey_Private4,
+  UKey_Private5,
+  UKey_Private6,
+  UKey_Private7,
+  UKey_Private8,
+  UKey_Private9,
+  UKey_Private10,
+  UKey_Private11,
+  UKey_Private12,
+  UKey_Private13,
+  UKey_Private14,
+  UKey_Private15,
+  UKey_Private16,
+  UKey_Private17,
+  UKey_Private18,
+  UKey_Private19,
+  UKey_Private20,
+  UKey_Private21,
+  UKey_Private22,
+  UKey_Private23,
+  UKey_Private24,
+  UKey_Private25,
+  UKey_Private26,
+  UKey_Private27,
+  UKey_Private28,
+  UKey_Private29,
+  UKey_Private30,
+
+  /* modifier keys */
+  UKey_Shift = 0x8000,
+  UKey_Control,
+  UKey_Alt,
+  UKey_Meta,
+  UKey_Super,
+  UKey_Hyper,
+
+  /* lock modifier keys: unstable */
+  UKey_Caps_Lock = 0x9000,
+  UKey_Num_Lock,
+  UKey_Scroll_Lock,
+
+#if 1
+  /* Deprecated. Please replace with UKey_Shift and so on. */
+  UKey_Shift_key = UKey_Shift,
+  UKey_Control_key = UKey_Control,
+  UKey_Alt_key = UKey_Alt,
+  UKey_Meta_key = UKey_Meta,
+  UKey_Super_key = UKey_Super,
+  UKey_Hyper_key = UKey_Hyper,
+#endif
+
+  UKey_Other = 0x10000
+};
+  
+enum UKeyModifier {
+  UMod_Shift = 1,
+  UMod_Control = 2,
+  UMod_Alt = 4,
+  UMod_Meta = 8,
+  UMod_Pseudo0 = 16,
+  UMod_Pseudo1 = 32,
+  UMod_Super = 64,
+  UMod_Hyper = 128
+};
+  
+enum UPreeditAttr {
+  UPreeditAttr_None = 0,
+  UPreeditAttr_UnderLine = 1,
+  UPreeditAttr_Reverse = 2,
+  UPreeditAttr_Cursor = 4,
+  UPreeditAttr_Separator = 8
+};
+
+/* Cursor of clipboard text is always positioned at end. */
+enum UTextArea {
+  UTextArea_Unspecified = 0,
+  UTextArea_Primary     = 1,  /* primary text area which IM commits to */
+  UTextArea_Selection   = 2,  /* user-selected region of primary text area */
+  UTextArea_Clipboard   = 4   /* clipboard text */
+};
+
+enum UTextOrigin {
+  UTextOrigin_Unspecified = 0,
+  UTextOrigin_Cursor      = 1,  /* current position of the cursor */
+  UTextOrigin_Beginning   = 2,  /* beginning of the text */
+  UTextOrigin_End         = 3   /* end of the text */
+};
+
+enum UTextExtent {
+  UTextExtent_Unspecified = -1,  /* invalid */
+
+  /* logical extents */
+  UTextExtent_Full      = -2,   /* beginning or end of the whole text */
+  UTextExtent_Paragraph = -3,   /* the paragraph which the origin is included */
+  UTextExtent_Sentence  = -5,   /* the sentence which the origin is included */
+  UTextExtent_Word      = -9,   /* the word which the origin is included */
+  UTextExtent_CharFrags = -17,  /* character fragments around the origin */
+
+  /* physical extents */
+  UTextExtent_DispRect  = -33,  /* the text region displayed in the widget */
+  UTextExtent_DispLine  = -65,  /* displayed line (eol: linebreak) */
+  UTextExtent_Line      = -129  /* real line      (eol: newline char) */
+};
+
+#endif // UIM_DPH_H__
